a71e8a4d251f5d331236c916f80bd300816a1783
jjtafur <jjtafur22@gmail.com>
2023-03-09 15:02:52 -0500

    Ejercicio2.ValidadorMayoriaEdad

---
+++ Ejercicio2.ValidadorMayoriaEdad/Ejercicio2.ValidadorMayoriaEdad/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio2.ValidadorMayoriaEdad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
