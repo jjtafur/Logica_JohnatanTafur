94bd765e900fbdde7e99deef6dbd26740bea7597
jjtafur <jjtafur22@gmail.com>
2023-04-13 18:38:36 -0500

    Commint2

--- Ejercicio24.Seguimiento/Ejercicio24.Seguimiento/Program.cs
+++ Ejercicio24.Seguimiento/Ejercicio24.Seguimiento/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 
 namespace Ejercicio24.Seguimiento
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int days, months, years;
+            string back;
+            Console.WriteLine("Ingrese número de días");
+            days = Convert.ToInt32(Console.ReadLine());
+
+            calculates(days);
+
+            Console.WriteLine("Presione Y para ingresar otro número ó N para salir");
+            back = Console.ReadLine();
+
+            if (back.ToUpper() != "S")
+            {
+          
+            }
+        }
+
+        static void calculates(int days)
+        {
+            
         }
     }
 }
