6ea043128f7a1f3934348d0d1f16c5c0f52b4c2d
jjtafur <jjtafur22@gmail.com>
2023-04-13 18:26:54 -0500

    Commint1

---
+++ Ejercicio24.Seguimiento/Ejercicio24.Seguimiento/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio24.Seguimiento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
