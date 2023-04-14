922f0c1e5ab998b6fc553ab742a08a3d5f9e38af
jjtafur <jjtafur22@gmail.com>
2023-04-13 19:36:11 -0500

    Commint4

--- Ejercicio24.Seguimiento/Ejercicio24.Seguimiento/Program.cs
+++ Ejercicio24.Seguimiento/Ejercicio24.Seguimiento/Program.cs
@@ -6,22 +6,26 @@ namespace Ejercicio24.Seguimiento
     public class Program
     {
         static void Main(string[] args)
+
         {
-            int days, months, years, numberdays;
-            string back;
-            Console.WriteLine("Ingrese número de días");
-            days = Convert.ToInt32(Console.ReadLine());
-
-            calculates(days, out years, out months, out numberdays);
-            showresult(days, years, months, numberdays);
-
-            Console.WriteLine("Presione Y para ingresar otro número ó N para salir");
-            back = Console.ReadLine();
-
-            if (back.ToUpper() != "S")
+            do
             {
-          
-            }
+                int days, months, years, numberdays;
+                Console.WriteLine("Ingrese número a evaluar");
+                days = Convert.ToInt32(Console.ReadLine());
+
+                calculates(days, out years, out months, out numberdays);
+                showresult(days, years, months, numberdays);
+
+                Console.WriteLine("Presione S para ingresar otro número ó N para salir");
+                string back = Console.ReadLine();
+
+                if (back.ToUpper() != "S")
+                {
+                    break;
+                }
+
+            } while (true);
         }
 
         static void calculates(int days, out int years, out int months, out int numberdays)
@@ -33,7 +37,7 @@ namespace Ejercicio24.Seguimiento
 
         static void showresult(int days, int years, int months, int numberdays)
         {
-            Console.WriteLine(String.Format("El número {0} contiene Años: {1}, Meses {2} Días {3}", days, years, months, numberdays));
+            Console.WriteLine(String.Format("El número {0} contiene\n Años: {1}\n Meses: {2}\n Días: {3}", days, years, months, numberdays));
         }
 
     }
