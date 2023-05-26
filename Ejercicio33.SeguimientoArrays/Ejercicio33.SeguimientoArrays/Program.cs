f8e4e901b5a504d9e1ecfd31397a217aa061975a
jjtafur <jjtafur22@gmail.com>
2023-05-25 21:32:24 -0500

    Commint.4

--- Ejercicio33.SeguimientoArrays/Ejercicio33.SeguimientoArrays/Program.cs
+++ Ejercicio33.SeguimientoArrays/Ejercicio33.SeguimientoArrays/Program.cs
@@ -12,21 +12,25 @@ namespace Ejercicio33.SeguimientoArrays
             int[] vector2 = new int[5];
             int[] vector3 = new int[5];
 
-            Console.WriteLine("Ingrese los elementos del vector 1");
+            Console.WriteLine("Ingrese los numeros del vector 1");
             LeerVector(vector1);
 
-            Console.WriteLine("Ingrese los elementos del vector 2");
+            Console.WriteLine("Ingrese los numeros del vector 2");
             LeerVector(vector2);
 
             CalcularSuma(vector1, vector2, vector3);
+
+            Console.WriteLine("El vector 3 es:"); MostrarVector(vector3);
+            Console.ReadLine();
+
         }
 
         static void LeerVector(int[] vector)
         {
-            for (int i=0; i< vector.Length; i++)
+            for (int i = 0; i < vector.Length; i++)
             {
-                Console.WriteLine($"Ingrese el elemento {i+1}");
-                vector[i]=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Ingrese la cifra {i + 1}: ");
+                vector[i] = Convert.ToInt32(Console.ReadLine());
             }
 
         }
@@ -36,13 +40,18 @@ namespace Ejercicio33.SeguimientoArrays
 
             for (int i = 0; i < vector1.Length; i++)
             {
-                vector3 = vector1 + vector2;
+                vector3[i] = vector1[i] + vector2[i];
             }
         }
 
-        static void MostrarVector()
+        static void MostrarVector(int[] vector)
         {
-        
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine(vector[i]);
+            }
+
+            Console.WriteLine();
         }
     }
 }
