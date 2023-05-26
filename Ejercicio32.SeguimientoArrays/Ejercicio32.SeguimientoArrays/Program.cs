b21c84b7bef085b154e16cc8f290393ba546c2d9
jjtafur <jjtafur22@gmail.com>
2023-05-25 20:42:47 -0500

    Commint.1

---
+++ Ejercicio32.SeguimientoArrays/Ejercicio32.SeguimientoArrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio32.SeguimientoArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
