3098b80a73965e14f8fa9a2e40e0b5f0e26bd82e
jjtafur <jjtafur22@gmail.com>
2023-05-25 21:17:59 -0500

    Commint.2

--- Ejercicio33.SeguimientoArrays/Ejercicio33.SeguimientoArrays/Program.cs
+++ Ejercicio33.SeguimientoArrays/Ejercicio33.SeguimientoArrays/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Ejercicio33.SeguimientoArrays
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vector1 = new int[5];
+            int[] vector2 = new int[5];
+            int[] vector3 = new int[5];
+
+            Console.WriteLine("Ingrese los elementos del vector 1");
+            LeerVector(vector1);
+
+            Console.WriteLine("Ingrese los elementos del vector 2");
+            LeerVector(vector2);
+
+        }
+
+        static void LeerVector(int[] vector)
+        {
+            for (int i=0; i< vector.Length; i++)
+            {
+                Console.WriteLine($"Ingrese el elemento {i+1}");
+                vector[i]=Convert.ToInt32(Console.ReadLine());
+            }
+
+        }
+
+        static void CalcularSuma()
+        {
+ 
+        }
+
+        static void MostrarVector()
+        {
+        
         }
     }
 }
